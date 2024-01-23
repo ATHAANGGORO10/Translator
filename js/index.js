31cfca7e5b45a99743c0298f82306b48efd1d4d3
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2024-01-23 18:18:19 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,122 @@
-const fromText = document.querySelector(".from-text"),
-    toText = document.querySelector(".to-text"),
-    exchageIcon = document.querySelector(".exchange"),
-    selectTag = document.querySelectorAll("select"),
-    icons = document.querySelectorAll(".row i");
-    translateBtn = document.querySelector("button"),
+const countries = {
+    "am-ET": "Amharic",
+    "ar-SA": "Arabic",
+    "be-BY": "Bielarus",
+    "bem-ZM": "Bemba",
+    "bi-VU": "Bislama",
+    "bjs-BB": "Bajan",
+    "bn-IN": "Bengali",
+    "bo-CN": "Tibetan",
+    "br-FR": "Breton",
+    "bs-BA": "Bosnian",
+    "ca-ES": "Catalan",
+    "cop-EG": "Coptic",
+    "cs-CZ": "Czech",
+    "cy-GB": "Welsh",
+    "da-DK": "Danish",
+    "dz-BT": "Dzongkha",
+    "de-DE": "German",
+    "dv-MV": "Maldivian",
+    "el-GR": "Greek",
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "et-EE": "Estonian",
+    "eu-ES": "Basque",
+    "fa-IR": "Persian",
+    "fi-FI": "Finnish",
+    "fn-FNG": "Fanagalo",
+    "fo-FO": "Faroese",
+    "fr-FR": "French",
+    "gl-ES": "Galician",
+    "gu-IN": "Gujarati",
+    "ha-NE": "Hausa",
+    "he-IL": "Hebrew",
+    "hi-IN": "Hindi",
+    "hr-HR": "Croatian",
+    "hu-HU": "Hungarian",
+    "id-ID": "Indonesian",
+    "is-IS": "Icelandic",
+    "it-IT": "Italian",
+    "ja-JP": "Japanese",
+    "kk-KZ": "Kazakh",
+    "km-KM": "Khmer",
+    "kn-IN": "Kannada",
+    "ko-KR": "Korean",
+    "ku-TR": "Kurdish",
+    "ky-KG": "Kyrgyz",
+    "la-VA": "Latin",
+    "lo-LA": "Lao",
+    "lv-LV": "Latvian",
+    "men-SL": "Mende",
+    "mg-MG": "Malagasy",
+    "mi-NZ": "Maori",
+    "ms-MY": "Malay",
+    "mt-MT": "Maltese",
+    "my-MM": "Burmese",
+    "ne-NP": "Nepali",
+    "niu-NU": "Niuean",
+    "nl-NL": "Dutch",
+    "no-NO": "Norwegian",
+    "ny-MW": "Nyanja",
+    "ur-PK": "Pakistani",
+    "pau-PW": "Palauan",
+    "pa-IN": "Panjabi",
+    "ps-PK": "Pashto",
+    "pis-SB": "Pijin",
+    "pl-PL": "Polish",
+    "pt-PT": "Portuguese",
+    "rn-BI": "Kirundi",
+    "ro-RO": "Romanian",
+    "ru-RU": "Russian",
+    "sg-CF": "Sango",
+    "si-LK": "Sinhala",
+    "sk-SK": "Slovak",
+    "sm-WS": "Samoan",
+    "sn-ZW": "Shona",
+    "so-SO": "Somali",
+    "sq-AL": "Albanian",
+    "sr-RS": "Serbian",
+    "sv-SE": "Swedish",
+    "sw-SZ": "Swahili",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+    "tet-TL": "Tetum",
+    "tg-TJ": "Tajik",
+    "th-TH": "Thai",
+    "ti-TI": "Tigrinya",
+    "tk-TM": "Turkmen",
+    "tl-PH": "Tagalog",
+    "tn-BW": "Tswana",
+    "to-TO": "Tongan",
+    "tr-TR": "Turkish",
+    "uk-UA": "Ukrainian",
+    "uz-UZ": "Uzbek",
+    "vi-VN": "Vietnamese",
+    "wo-SN": "Wolof",
+    "xh-ZA": "Xhosa",
+    "yi-YD": "Yiddish",
+    "zu-ZA": "Zulu"
+}
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTags = document.querySelectorAll("select");
+const translateBtn = document.querySelector("button");
+selectTags.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id === 0 ? (country_code === "en-GB" ? "selected" : "") : (country_code === "hi-IN" ? "selected" : "");
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
 
-    selectTag.forEach((tag, id) => {
-        for (let country_code in countries) {
-            let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-            let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-            tag.insertAdjacentHTML("beforeend", option);
-        }
-    });
-
-exchageIcon.addEventListener("click", () => {
-    let tempText = fromText.value,
-        tempLang = selectTag[0].value;
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value;
+    let tempLang = selectTags[0].value;
     fromText.value = toText.value;
     toText.value = tempText;
-    selectTag[0].value = selectTag[1].value;
-    selectTag[1].value = tempLang;
+    selectTags[0].value = selectTags[1].value;
+    selectTags[1].value = tempLang;
 });
 
 fromText.addEventListener("keyup", () => {
@@ -29,42 +126,31 @@ fromText.addEventListener("keyup", () => {
 });
 
 translateBtn.addEventListener("click", () => {
-    let text = fromText.value.trim(),
-        translateFrom = selectTag[0].value,
-        translateTo = selectTag[1].value;
-    if (!text) return;
-    toText.setAttribute("placeholder", "Translating...");
-    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
-    fetch(apiUrl).then(res => res.json()).then(data => {
-        toText.value = data.responseData.translatedText;
-        data.matches.forEach(data => {
-            if (data.id === 0) {
-                toText.value = data.translation;
-            }
+    let text = fromText.value.trim();
+    let translateFrom = selectTags[0].value;
+    let translateTo = selectTags[1].value;
+    if (!text) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Tolong isi terlebih dahulu',
+            showConfirmButton: false,
+            timer: 1500
         });
-        toText.setAttribute("placeholder", "Translation");
-    });
-});
-
-icons.forEach(icon => {
-    icon.addEventListener("click", ({ target }) => {
-        if (!fromText.value || !toText.value) return;
-        if (target.classList.contains("fa-copy")) {
-            if (target.id == "from") {
-                navigator.clipboard.writeText(fromText.value);
-            } else {
-                navigator.clipboard.writeText(toText.value);
-            }
-        } else {
-            let utterance;
-            if (target.id == "from") {
-                utterance = new SpeechSynthesisUtterance(fromText.value);
-                utterance.lang = selectTag[0].value;
-            } else {
-                utterance = new SpeechSynthesisUtterance(toText.value);
-                utterance.lang = selectTag[1].value;
-            }
-            speechSynthesis.speak(utterance);
-        }
-    });
+        return;
+    }
+    toText.setAttribute("placeholder", "Translating...");
+    setTimeout(() => {
+        let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+        fetch(apiUrl)
+            .then(res => res.json())
+            .then(data => {
+                toText.value = data.responseData.translatedText;
+                data.matches.forEach(data => {
+                    if (data.id === 0) {
+                        toText.value = data.translation;
+                    }
+                });
+                toText.setAttribute("placeholder", "Translation");
+            });
+    }, 1000);
 });
